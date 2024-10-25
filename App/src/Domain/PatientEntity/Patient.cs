13d8bf5fd3ba291b3c30fef88b863ecc336a5400
Tomás Leite <1220979@isep.ipp.pt>
2024-10-25 22:35:10 +0100

    #32 - Edited patient class

--- App/src/Domain/PatientEntity/Patient.cs
+++ App/src/Domain/PatientEntity/Patient.cs
@@ -1,18 +1,36 @@
 using Sempi5.Domain.Shared;
 using Sempi5.Domain.UserEntity;
+using System;
+using System.Collections.Generic;
 
 namespace Sempi5.Domain.PatientEntity
 {
     public class Patient : Entity<PatientID>, IAggregateRoot
     {
-        public DateTime DateOfBirth { get; set; }
+        private DateTime _dateOfBirth;
+
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set => ChangeBirthDate(value);
+        }
+
         public Name Name { get; set; }
         public Gender Gender { get; set; }
         public Email Email { get; set; }
-        public Phone Phone { get; set; }  
+        public Phone Phone { get; set; }
         public Address Address { get; set; }
-        public List<Condition> Conditions { get; set; }  
+        public List<Condition> Conditions { get; set; }
         public Phone EmergencyContact { get; set; }
         public SystemUser? SystemUser { get; set; }
+
+        public void ChangeBirthDate(DateTime newDate)
+        {
+            if (newDate > DateTime.Now)
+            {
+                throw new BusinessRuleValidationException("Date of birth cannot be in the future");
+            }
+            _dateOfBirth = newDate;
+        }
     }
 }
