69758eabc5ec0f6f2a0f7fb956d5c4564f5b62be
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-14 19:19:47 +0000

    #89 Enhance SpecializationRepository: Add asynchronous methods for adding, retrieving by ID and name, and updating specializations

--- BackOffice/App/src/Infrastructure/SpecializationRepository/SpecializationRepository.cs
+++ BackOffice/App/src/Infrastructure/SpecializationRepository/SpecializationRepository.cs
@@ -13,8 +13,31 @@ namespace Sempi5.Infrastructure.SpecializationRepository
         private readonly DataBaseContext _context;
         public SpecializationRepository(DataBaseContext context) : base(context.Specializations)
         {
-            _context = context;
+                _context = context;
+            }
+    
+            public async Task AddAsync(Specialization specialization)
+            {
+                await _context.Specializations.AddAsync(specialization);
+                await _context.SaveChangesAsync();
+            }
+    
+            public async Task<Specialization?> GetByIdAsync(SpecializationID id)
+            {
+                return await _context.Specializations.FindAsync(id);
+            }
+
+        public Task<Specialization?> GetByName(string name)
+        {
+            return _context.Specializations.FirstOrDefaultAsync(x => x.Name == name);
         }
 
+        public async Task UpdateAsync(Specialization specialization)
+            {
+                _context.Specializations.Update(specialization);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        
     }
-}
