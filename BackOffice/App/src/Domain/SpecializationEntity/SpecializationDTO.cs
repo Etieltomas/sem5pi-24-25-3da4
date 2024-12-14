88db303c7ddad58fa9623cc89740614a9fdcfed3
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-14 19:19:14 +0000

    #89 Add DTO classes for Specialization creation and update

---
+++ BackOffice/App/src/Domain/SpecializationEntity/SpecializationDTO.cs
@@ -0,0 +1,8 @@
+namespace Sempi5.Domain.SpecializationEntity{
+    public class SpecializationCreateDTO{
+        public string Name { get; set; }
+    }
+    public class SpecializationUpdateDTO{
+        public string Name { get; set; }
+    }
+}
