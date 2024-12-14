957c9956089ed31985ff810fb8e50810d1572c65
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-14 19:18:56 +0000

    #89 Enhance ISpecializationRepository: Add asynchronous methods for adding, retrieving, and updating specializations

--- BackOffice/App/src/Domain/SpecializationEntity/ISpecializationRepository.cs
+++ BackOffice/App/src/Domain/SpecializationEntity/ISpecializationRepository.cs
@@ -4,5 +4,11 @@ namespace Sempi5.Domain.SpecializationEntity
 {
     public interface ISpecializationRepository : IRepository<Specialization,SpecializationID>
     {
+
+        Task AddAsync(Specialization specialization);
+        Task<List<Specialization>> GetAllAsync();
+        Task<Specialization?> GetByIdAsync(SpecializationID id);
+        Task UpdateAsync(Specialization specialization);
+        Task<Specialization?> GetByName(string name);
     }
 }
