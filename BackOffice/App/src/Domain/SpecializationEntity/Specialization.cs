154ed84abde57c46176fb09be44e22648bbb41be
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-19 11:36:32 +0000

    #89 'Add Specializations' - Enhance Specialization entity: add Code and Description properties, and update method to handle multiple parameters

--- BackOffice/App/src/Domain/SpecializationEntity/Specialization.cs
+++ BackOffice/App/src/Domain/SpecializationEntity/Specialization.cs
@@ -5,10 +5,14 @@ namespace Sempi5.Domain.SpecializationEntity
     public class Specialization : Entity<SpecializationID>, IAggregateRoot
     {
         public string Name { get; set; }
-
-        public void UpdateName(string newName)
+        public string Code { get; set; } 
+        public string? Description { get; set; }  
+        public void Update(string newName, string newCode, string? newDescription)
         {
             Name = newName;
+            Code = newCode;
+            Description = newDescription;
         }
     }
 }
+
