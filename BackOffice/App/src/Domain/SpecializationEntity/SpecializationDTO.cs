bce1df30c202443cc7c75015e19aa4647a391f60
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-19 11:36:42 +0000

    #89 'Add Specializations' - Add Specialization DTOs: enhance creation and update DTOs with Code and Description properties, and introduce a new SpecializationDTO class

--- BackOffice/App/src/Domain/SpecializationEntity/SpecializationDTO.cs
+++ BackOffice/App/src/Domain/SpecializationEntity/SpecializationDTO.cs
@@ -1,8 +1,24 @@
-namespace Sempi5.Domain.SpecializationEntity{
-    public class SpecializationCreateDTO{
+namespace Sempi5.Domain.SpecializationEntity
+{
+    public class SpecializationCreateDTO
+    {
         public string Name { get; set; }
+        public string Code { get; set; }
+        public string? Description { get; set; }
     }
-    public class SpecializationUpdateDTO{
+
+    public class SpecializationUpdateDTO
+    {
         public string Name { get; set; }
+        public string Code { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class SpecializationDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string? Description { get; set; }
     }
 }
