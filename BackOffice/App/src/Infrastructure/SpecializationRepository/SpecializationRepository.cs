c8cd630ccc883825a70369459baa8146e565c0cd
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-19 11:37:02 +0000

    #89 'Add Specializations' - Refactor SpecializationRepository: streamline GetByName method and remove unused methods

--- BackOffice/App/src/Infrastructure/SpecializationRepository/SpecializationRepository.cs
+++ BackOffice/App/src/Infrastructure/SpecializationRepository/SpecializationRepository.cs
@@ -1,8 +1,5 @@
-
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sempi5.Domain.SpecializationEntity;
-using Sempi5.Domain.UserEntity;
 using Sempi5.Infrastructure.Databases;
 using Sempi5.Infrastructure.Shared;
 
@@ -11,33 +8,16 @@ namespace Sempi5.Infrastructure.SpecializationRepository
     public class SpecializationRepository : BaseRepository<Specialization, SpecializationID>, ISpecializationRepository
     {
         private readonly DataBaseContext _context;
+
         public SpecializationRepository(DataBaseContext context) : base(context.Specializations)
         {
-                _context = context;
-            }
-    
-            public async Task AddAsync(Specialization specialization)
-            {
-                await _context.Specializations.AddAsync(specialization);
-                await _context.SaveChangesAsync();
-            }
-    
-            public async Task<Specialization?> GetByIdAsync(SpecializationID id)
-            {
-                return await _context.Specializations.FindAsync(id);
-            }
+            _context = context;
+        }
 
-        public Task<Specialization?> GetByName(string name)
+        public async Task<Specialization> GetByName(string name)
         {
-            return _context.Specializations.FirstOrDefaultAsync(x => x.Name == name);
-        }
 
-        public async Task UpdateAsync(Specialization specialization)
-            {
-                _context.Specializations.Update(specialization);
-                await _context.SaveChangesAsync();
-            }
+            return await _context.Specializations.FirstOrDefaultAsync(rt => rt.Name.Equals(name));
         }
-
-        
     }
+}
