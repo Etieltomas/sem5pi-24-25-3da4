9877d1657b9d74907c5c815835ec5ebf78381545
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-19 11:37:19 +0000

    #89 'Add Specializations' - Refactor ISpecializationRepository: simplify GetByName method signature and remove unused methods

--- BackOffice/App/src/Domain/SpecializationEntity/ISpecializationRepository.cs
+++ BackOffice/App/src/Domain/SpecializationEntity/ISpecializationRepository.cs
@@ -2,13 +2,8 @@ using Sempi5.Domain.Shared;
 
 namespace Sempi5.Domain.SpecializationEntity
 {
-    public interface ISpecializationRepository : IRepository<Specialization,SpecializationID>
+    public interface ISpecializationRepository : IRepository<Specialization, SpecializationID>
     {
-
-        Task AddAsync(Specialization specialization);
-        Task<List<Specialization>> GetAllAsync();
-        Task<Specialization?> GetByIdAsync(SpecializationID id);
-        Task UpdateAsync(Specialization specialization);
-        Task<Specialization?> GetByName(string name);
+        Task<Specialization> GetByName(string name);
     }
 }
