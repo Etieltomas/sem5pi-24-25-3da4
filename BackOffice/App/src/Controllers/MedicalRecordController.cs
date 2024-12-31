03780da5729782f6a30cdcd85c9a7d5997ca11a9
Simao Lopes <1221173@isep.ipp.pt>
2024-12-31 01:57:40 +0000

    #4 (Update Medical Record) - Add function to get a specific medical record entry and to update an entry. Also add function to add a new entry in medical record

--- BackOffice/App/src/Controllers/MedicalRecordController.cs
+++ BackOffice/App/src/Controllers/MedicalRecordController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Sempi5.Domain.MedicalRecordEntity;
@@ -11,10 +12,12 @@ namespace Sempi5.Controllers
     {
 
         private readonly MedicalRecordService _service;
+        private readonly ILogger<MedicalRecordController> _logger;
 
-        public MedicalRecordController(MedicalRecordService service)
+        public MedicalRecordController(MedicalRecordService service, ILogger<MedicalRecordController> logger)
         {
             _service = service;
+            _logger = logger;
         }
 
         [HttpGet("{patientEmail}")]
@@ -29,17 +32,29 @@ namespace Sempi5.Controllers
 
             return Ok(result);
         }
-        
 
+        [HttpGet("entry/{idMedicalRecord}")]
+        [Authorize(Roles = "Staff, Doctor")]
+        public async Task<ActionResult<List<MedicalRecordDTO>>> GetEntryMedicalRecord(string idMedicalRecord)
+        {
+            var result = await _service.GetEntryMedicalRecord(idMedicalRecord);
+            if (result == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+        
         [HttpGet]
-        [Authorize(Roles = "Staff")]
+        [Authorize(Roles = "Staff, Doctor")]
         public async Task<ActionResult<List<MedicalRecordDTO>>> GetAllMedicalRecords()
         {
             return Ok(await _service.GetAllMedicalRecords());
         }
 
         [HttpGet("search")]
-        [Authorize(Roles = "Staff")]
+        [Authorize(Roles = "Staff, Doctor")]
         public async Task<ActionResult<List<MedicalRecordDTO>>> Search(
             [FromQuery] string? filter,
             [FromQuery] string patient,
@@ -53,6 +68,55 @@ namespace Sempi5.Controllers
                 return BadRequest();
             }
 
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Doctor")]
+        public async Task<ActionResult<MedicalRecordDTO>> AddMedicalRecord([FromBody] MedicalRecordDTO medicalRecord)
+        {
+            var result = await _service.AddMedicalRecord(medicalRecord);
+            if (result == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+
+        /*[HttpPatch("entry/{idMedicalRecord}/update")]
+        [Authorize(Roles = "Staff, Doctor")]
+        public async Task<ActionResult<RecordLineDTO>> Update(string idMedicalRecord, [FromBody] RecordLineDTO medicalRecordLine)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _service.Update(medicalRecordLine, idMedicalRecord);
+            if (result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }*/
+
+        [HttpPatch("entry/{idMedicalRecord}/update")]
+        [Authorize(Roles = "Staff, Doctor")]
+        public async Task<ActionResult<MedicalRecordDTO>> Update(string idMedicalRecord, [FromBody] MedicalRecordDTO medicalRecord)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _service.Update(medicalRecord, idMedicalRecord);
+            if (result == null)
+            {
+                return BadRequest();
+            }
             return Ok(result);
         }
     }
