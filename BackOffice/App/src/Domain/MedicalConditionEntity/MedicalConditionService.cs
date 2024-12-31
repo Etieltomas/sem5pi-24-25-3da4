cfc5285624415112e614e6dffd2eafcab86f83b5
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-31 11:44:38 +0000

    #3 (Search Medical Condition) - Add MedicalConditionService with methods to add and retrieve medical conditions

--- BackOffice/App/src/Domain/MedicalConditionEntity/MedicalConditionService.cs
+++ BackOffice/App/src/Domain/MedicalConditionEntity/MedicalConditionService.cs
@@ -1,32 +1,28 @@
-using System.Net.Http;
-using System.Text.Json;
 using System.Text;
-using Microsoft.Extensions.Logging;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Sempi5.Domain.AllergyEntity
+namespace Sempi5.Domain.MedicalConditionEntity
 {
-    public class AllergyService
+    public class MedicalConditionService
     {
         private readonly IConfiguration _configuration;
         private readonly string base_url;
         private readonly HttpClient _httpClient;
-        private readonly ILogger<AllergyService> _logger;
 
-        // Constructor with HttpClient and ILogger dependency injection
-        public AllergyService(IConfiguration configuration, HttpClient httpClient, ILogger<AllergyService> logger)
+        // Constructor with HttpClient dependency injection
+        public MedicalConditionService(IConfiguration configuration, HttpClient httpClient)
         {
             _configuration = configuration;
             _httpClient = httpClient;
-            _logger = logger;
             base_url = _configuration["IpAddresses:BackEnd2"] ?? "http://localhost:3001";
         }
 
-        // Method to add a new allergy using DTO
-        public async Task<string> AddAllergy([FromBody] AllergyDTO allergyDTO)
+        // Method to add a new medical condition using DTO
+        public async Task<string> AddMedicalCondition([FromBody] MedicalConditionDTO medicalConditionDTO)
         {
-            var url = base_url+"/api/allergies";
+            var url = base_url + "/api/medical-conditions";
 
             var options = new JsonSerializerOptions
             {
@@ -34,7 +30,7 @@ namespace Sempi5.Domain.AllergyEntity
             };
 
             // Create the JSON payload
-            var content = new StringContent(JsonSerializer.Serialize(allergyDTO, options), Encoding.UTF8, "application/json");
+            var content = new StringContent(JsonSerializer.Serialize(medicalConditionDTO, options), Encoding.UTF8, "application/json");
 
             // Make the POST request
             var response = await _httpClient.PostAsync(url, content);
@@ -44,42 +40,41 @@ namespace Sempi5.Domain.AllergyEntity
                 // Return null if the request was not successful
                 return null;
             }
+
             // Return the response body as a string
             return await response.Content.ReadAsStringAsync();
         }
 
-        // Method to get all allergies as a list of DTOs
-        public async Task<List<AllergyDTO>> GetAllAllergies()
+        // Method to get all medical conditions as a list of DTOs
+        public async Task<List<MedicalConditionDTO>> GetAllMedicalConditions()
         {
-            var url = base_url + "/api/allergies"; 
+            var url = base_url + "/api/medical-conditions";
 
             // Make the GET request
             var response = await _httpClient.GetAsync(url);
 
             if (!response.IsSuccessStatusCode)
             {
-                // Return null if the request was not successful
-                return null;
+                // Return an empty list if the request was not successful
+                return new List<MedicalConditionDTO>();
             }
 
             // Deserialize the JSON response
             var json = await response.Content.ReadAsStringAsync();
-
             var options = new JsonSerializerOptions
             {
                 DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
                 PropertyNameCaseInsensitive = true,
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
-
             using (JsonDocument doc = JsonDocument.Parse(json))
             {
                 // Extract the _value property
                 var valueElement = doc.RootElement.GetProperty("_value");
-
-                // Deserialize the _value property to a list of AllergyDTO
-                var allergies = JsonSerializer.Deserialize<List<AllergyDTO>>(valueElement.GetRawText(), options);
-                return allergies;
+            
+                 // Deserialize the _value property to a list of AllergyDTO
+                var medicalConditions = JsonSerializer.Deserialize<List<MedicalConditionDTO>>(valueElement.GetRawText(), options);
+                return medicalConditions;
             }
         }
     }
