726b6dd92504b9c89590daba4a0ded6e1ddf11f6
Ricardo Guimarães <1212125@isep.ipp.pt>
2024-12-31 11:44:49 +0000

    #3 (Search Medical Condition) - Add MedicalConditionDTO class to represent medical condition data

---
+++ BackOffice/App/src/Domain/MedicalConditionEntity/MedicalConditionDTO.cs
@@ -0,0 +1,9 @@
+namespace Sempi5.Domain.MedicalConditionEntity
+{
+    public class MedicalConditionDTO
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
