096e5d8d31617dcb7f7bc2cfc3b0009d8876284f
Simao Lopes <1221173@isep.ipp.pt>
2025-01-02 04:22:52 +0000

    #86 (Create Surgery Appointment) - Change dto of operation request: Priority is a string and all of the attributes are optional (make sure that a can create an DTO without an id)

--- BackOffice/App/src/Domain/OperationRequestEntity/OperationRequestDTO.cs
+++ BackOffice/App/src/Domain/OperationRequestEntity/OperationRequestDTO.cs
@@ -1,11 +1,11 @@
 public class OperationRequestDto
 {
-    public int Id { get; set; }
-    public string PatientName { get; set; }
-    public string OperationType { get; set; }
-    public int Priority { get; set; }
-    public DateTime Deadline { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public string Status { get; set; }
+    public int? Id { get; set; }
+    public string? PatientName { get; set; }
+    public string? OperationType { get; set; }
+    public string? Priority { get; set; }
+    public DateTime? Deadline { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string? Status { get; set; }
 }
